b52c8b64aa948d660fabcd557e9f3d0d6a18e553
Dominik Schreiber <mail@dominikschreiber.de>
2026-03-17 16:16:31 +0100

    Improve kitten rename script

--- rename-kitten-symbols.sh
+++ rename-kitten-symbols.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-grep -oE "[ck]itten[A-Za-z_0-9]+" src/kitten.h | while read x ; do
-	sed -i 's/\b'$x'\b/cadi'$x'/g' src/*.{h,c,hpp,cpp}
+(grep -oE "[ck]itten[A-Za-z_0-9]+" src/kitten.h ;
+	echo "new_learned_klause" ;
+	echo "completely_backtrack_to_root_level" ) | while read x ; do
+	sed -i 's/\b'$x'\b/cadi_'$x'/g' src/*.{h,c,hpp,cpp}
 done
 
